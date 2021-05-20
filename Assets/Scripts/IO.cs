77d34eb7aaa911d816fa64af95ea6c16388338de
Skoftig <saraderooij1@gmail.com>
2021-05-20 21:02:07 +0200

    implemented highscore system

--- Assets/Scripts/IO.cs
+++ Assets/Scripts/IO.cs
@@ -1,24 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class IO : MonoBehaviour
 {
+    [HideInInspector]
     public List<int> highscore = new List<int>();
+    private string filePath = "Assets/Highscore.txt";
+
 
     /// <summary>
     /// this function writes the game highscore.
     /// </summary>
     public void WriteHighScore()
     {
-        using (StreamWriter sw = new StreamWriter("Highscore.txt"))
+        using (StreamWriter sw = new StreamWriter(filePath))
         {
             for (int i = 0; i < highscore.Count; i++)
             {
                 sw.WriteLine(highscore[i].ToString());
             }
+
         }
+
+        AssetDatabase.Refresh();
     }
 
     public List<int> ReadHighScore()
@@ -26,7 +33,7 @@ public class IO : MonoBehaviour
         string line = "";
         highscore = new List<int>();
 
-        using (StreamReader sr = new StreamReader("Highscore.txt"))
+        using (StreamReader sr = new StreamReader(filePath))
         {
             while ((line = sr.ReadLine()) != null)
             {
@@ -39,6 +46,6 @@ public class IO : MonoBehaviour
             }
         }
 
-                return highscore;
+      return highscore;
     }
 }
